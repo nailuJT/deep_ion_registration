f85f559582757f40ed22a34ab8e4238dda4aaa46
Julian Titze <Julian Titze>
2023-12-12 10:25:36 +0100

    [CODE] refined gaussian transform on data

--- datapipe/data_transform.py
+++ datapipe/data_transform.py
@@ -1,11 +1,18 @@
 import numpy as np
 import os
 from scipy.ndimage import map_coordinates
+import warnings
+def gaussian(x, mu, sigma, alpha, epsilon=1e-8):
+    """
+    Computes a Gaussian function.
+    """
 
-def gaussian(x, mu, sig):
-    y = np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+    dist = np.power(x - mu[:, None, None], 2)
+    dist = np.divide(dist, 2 * sigma[:, None, None] ** 2 + epsilon)
+    dist = np.sum(dist, axis=0)
+    y = alpha * np.exp(-dist)
+
     return y
-
 
 def sample_gaussian_parameters(center_mu,
                                center_sigma,
@@ -38,49 +45,89 @@ def sample_gaussian_parameters(center_mu,
 
     return parameters
 
-
-
-def apply_gaussian_transform(image, epsilon, mu, sigma, lensing=True):
+def gaussian_shift(coordinates, image_center, alpha, mu, sigma, dimension, lensing=False):
     """
     Applies a Gaussian transform to an image.
     """
-    center = (np.floor(image.shape[0] / 2), np.floor(image.shape[1] / 2))
-
-    # Create a Gaussian vector field
-    y_indices, x_indices = np.indices(image.shape)
-
-    sign = [1, 1]
-    lensing = True
+    mu_normalized = mu + image_center
+    shift = gaussian(coordinates, mu_normalized, sigma, alpha)
 
     if lensing:
-        sign[0] = np.where(x_indices < center[1], -1, 1)
-        sign[1] = np.where(y_indices < center[0], -1, 1)
+        sign = np.where(coordinates[dimension] < mu_normalized[dimension], -1., 1.)
+        shift = shift * sign
 
-    vector_field = np.stack([
-        sign[0] * epsilon[0] * gaussian(x_indices, mu[0] + center[0], sigma[0]),
-        sign[1] * epsilon[1] * gaussian(y_indices, mu[1] + center[1], sigma[1])
-    ], axis=0)
+    return shift
 
-    coordinates = np.stack([y_indices, x_indices], axis=0)
-    vector_field = vector_field + coordinates
+def gaussian_derivative(coordinates, image_center, alpha, mu, sigma, dimension):
+
+    mu_normalized = mu + image_center
+    shift = gaussian(coordinates, mu_normalized, sigma, alpha)
+    shift = shift * (coordinates[dimension] - mu_normalized[dimension]) / sigma[dimension] ** 2
+    rota
+    return shift
+
+def test_gaussian_shift():
+    """
+    Tests the gaussian_shift function.
+    :return:
+    """
+    coordinates = np. meshgrid(np.arange(10), np.arange(10))
+
+def apply_gaussian_transform(image, alpha_dirs, mu_dirs, sigma_dirs, **kwargs):
+    """
+    Applies a Gaussian transform to an image.
+    """
+
+    if not all([image.ndim == mu_dirs[i].shape[0] for i in range(len(alpha_dirs))]):
+        raise ValueError("Image and all directions must have the same first dimension size.")
+
+    image_center = np.array([np.floor(image.shape[i] / 2) for i in range(len(image.shape))])
+    shape = image.shape
+
+    coordinates = np.stack(np.indices(shape))
+    vector_field = []
+
+    for dimension, (alpha, mu, sigma, rotation)in enumerate(zip(alpha_dirs, mu_dirs, sigma_dirs, rotation_dirs)):
+
+        vector_field += [gaussian_derivative(coordinates=coordinates,
+                                        image_center=image_center,
+                                        alpha=alpha,
+                                        mu=mu,
+                                        sigma=sigma,
+                                        dimension=dimension,
+                                        **kwargs)]
+
+    vector_field = np.stack(vector_field, axis=0)
+    coordinates_transformed = vector_field + coordinates
 
     # Apply the Gaussian vector field to the image
-    transformed_image = map_coordinates(image, vector_field, order=1)
+    transformed_image = map_coordinates(image, coordinates_transformed, order=1)
 
-    return transformed_image
+    return transformed_image, vector_field
+
+def visualize_vector_field(vector_field):
+    import matplotlib.pyplot as plt
+
+    plt.quiver(vector_field[1, :, :], vector_field[0, :, :])
+    plt.show()
 
 def load_test_image():
     from phantom_helpers.binary_tools import read_binary, compare_images
+    try:
+        BASE_PATH = "/home/j/J.Titze/Projects/XCAT_data/Phantoms/"
+        postfix = "_atn_1.bin"
 
-    BASE_PATH = "/home/j/J.Titze/Projects/XCAT_data/Phantoms/"
-    postfix = "_atn_1.bin"
+        path_original = "high"
 
-    path_original = "high"
+        image_dimenstions = (512, 512, 40)
+        slice = 20
 
-    image_dimenstions = (512, 512, 40)
-    slice = 20
+        image_original = read_binary(os.path.join(BASE_PATH, path_original + postfix), *image_dimenstions)[slice, :, :]
 
-    image_original = read_binary(os.path.join(BASE_PATH, path_original + postfix), *image_dimenstions)[slice,:,:]
+    except FileNotFoundError:
+        warnings.warn("Could not find test image.\n "
+                      "Please download the XCAT phantom and set the correct path.")
+        image_original = np.ones((20, 20))
 
     return image_original
 
@@ -91,14 +138,20 @@ def test_apply_gaussian_transform():
     from phantom_helpers.binary_tools import compare_images
 
     gaussian_parameters = {
-        "epsilon": np.array([5, 5]),
-        "mu": np.array([20, 0]),
-        "sigma": np.array([10, 10])
+        "alpha_dirs": [1, 0.5],
+        "mu_dirs": np.array([[0, 0],
+                             [0, 0]]),
+        "sigma_dirs": [np.array([4, 4]),
+                       np.array([6, 6])]
     }
 
     image_original = load_test_image()
-    image_warped = apply_gaussian_transform(image_original, **gaussian_parameters)
-    compare_images(image_original, image_warped)
+
+    image_warped, vector_field = apply_gaussian_transform(image_original, **gaussian_parameters)
+
+    visualize_vector_field(vector_field)
+
+    #compare_images(image_original, image_warped)
 
 def test_sample_gaussian_transform():
     from phantom_helpers.binary_tools import compare_images
@@ -127,5 +180,5 @@ def test_sample_gaussian_transform():
 
 
 if __name__ == '__main__':
-    #test_apply_gaussian_transform()
-    test_sample_gaussian_transform()
+    test_apply_gaussian_transform()
+    #test_sample_gaussian_transform()
