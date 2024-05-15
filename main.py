cd8186980be70df3c44ab9b0558e62c2cb884bd6
Julian Titze <Julian Titze>
2024-05-15 12:42:30 +0200

    [CODE] added main

---
+++ main.py
@@ -0,0 +1,6 @@
+from deep_ion_registration.training.training_simple import loo_evaluation
+import sys; print('Python %s on %s' % (sys.version, sys.platform))
+sys.path.extend('/home/j/J.Titze/pycharm_remote/remote_10/')
+
+if __name__ == '__main__':
+    loo_evaluation()
